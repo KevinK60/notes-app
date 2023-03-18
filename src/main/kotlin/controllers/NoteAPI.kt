c7afe1bfc1d77316c545ce941801b1d934476f4f
kevin <kevin9000m@Gmail.com>
2023-03-18 14:56:18 +0000

    adds option to archive a note

--- src/main/kotlin/controllers/NoteAPI.kt
+++ src/main/kotlin/controllers/NoteAPI.kt
@@ -52,6 +52,16 @@ class NoteAPI(serializerType: Serializer) {
         return counter
     }
 
+    fun archiveNote(indexToArchive: Int): Boolean {
+        if (isValidIndex(indexToArchive)) {
+            val noteToArchive = notes[indexToArchive]
+            if (!noteToArchive.isNoteArchived) {
+                noteToArchive.isNoteArchived = true
+                return true
+            }
+        }
+        return false
+    }
 
     // Archived Notes
     fun numberOfArchivedNotes(): Int {
@@ -157,6 +167,12 @@ class NoteAPI(serializerType: Serializer) {
 
     fun isValidIndex(Index: Int): Boolean {
         return isValidListIndex(Index, notes);
+    }
+
+    companion object {
+        fun listArchivedNotes() {
+
+        }
     }
 
 
